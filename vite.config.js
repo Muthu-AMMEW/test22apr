e138696337bdaae7ac4db99400fe154752ef1b8f
Muthu-AMMEW <muthu.ammew@gmail.com>
2025-04-22 20:53:53 +0530

    update proxy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'My App',
+        short_name: 'App',
+        description: 'My Vite React App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/images/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/images/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ], server: {
+    proxy: {
+      '/api': {
+        target: 'https://ecommerce-backend-v9p4.onrender.com', // your backend
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
